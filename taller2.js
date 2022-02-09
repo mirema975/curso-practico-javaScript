8339e950170a6bbeb70748cf9c418faee7e83087
mirema975 <emanuelmiranda975@gmail.com>
2022-02-08 21:07:18 -0300

    add function inputsNumber, inputText, cupones

--- taller2.js
+++ taller2.js
@@ -1,20 +1,46 @@
+const coupons = [
+    "Descuento1",
+    "Descuento2",
+    "Descuento3"
+];
+
+function inputsNumber(input){
+    const inputV = document.getElementById(input);
+    const inputValue = Number(inputV.value);
+    return inputValue;
+}
+
+function inputsText(input){
+    const inputV = document.getElementById(input);
+    const inputValue = inputV.value;
+    return inputValue;
+}
+
 function calcularPrecioConDescuento(precio, descuento) {
     const precioConDescuentoAplicado = (precio * (100 - descuento)) / 100;
     return precioConDescuentoAplicado;
 }
 
-function precioPorcentajeAplicado() {
-    const inputPrecio = document.getElementById("inputPrecio");
-    const precio = inputPrecio.value;
+function cupones() {
 
-    const inputDescuento = document.getElementById("inputDescuento");
-    const descuento = inputDescuento.value;
+    const precioValue = inputsNumber("inputPrecio");
+    const descuentoValue = inputsText("inputDescuento");
 
-    const total = calcularPrecioConDescuento(precio, descuento);
-    
+    let descuentoCupon;
+
+    if(!coupons.includes(descuentoValue)){
+        alert("El cupon ingresado no es valido")
+    }else if (descuentoValue === "Descuento1"){
+        descuentoCupon = 15;
+    }else if (descuentoValue === "Descuento2"){
+        descuentoCupon = 20;
+    }else if (descuentoValue === "Descuento3"){
+        descuentoCupon = 25;
+    }
+
+    const total = calcularPrecioConDescuento(precioValue, descuentoCupon);
+
     const parrafoPrecio = document.getElementById("resultPrice");
     parrafoPrecio.innerText = "El precio final es de $" + total;
-    
-    return total;
 
 }
